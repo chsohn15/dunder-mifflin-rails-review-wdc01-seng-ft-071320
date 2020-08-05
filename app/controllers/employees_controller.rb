b5ad705d7cfd6e7831b35b82400ba74e814fa2cf
chsohn15 <chsohn15@gmail.com>
2020-08-05 14:55:55 -0400

    Created index and show pages

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,11 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        id = params[:id]
+        @employee = Employee.find(id)
+    end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,12 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def employees
+        # Go through employees, find the ones where dog_id = self.id
+        all_employees = Employee.where(dog_id: self.id)
+        employee_names = all_employees.map do |employee|
+            "#{employee.first_name} #{employee.last_name}"
+        end
+        employee_names.join(", ")
+    end
 end
